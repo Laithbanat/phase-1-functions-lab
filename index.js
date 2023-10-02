adde0754d5d2c4e8b2f513cda8867204baaca214
Laith banat <laithahmad201092@gmail.com>
2023-10-02 14:23:23 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264; // Assuming 1 block = 264 feet
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  const distance = distanceTravelledInFeet(startBlock, endBlock);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return 'Ride not allowed';
+  }
+}
+
+module.exports = {
+  distanceFromHqInBlocks,
+  distanceFromHqInFeet,
+  distanceTravelledInFeet,
+  calculatesFarePrice,
+};
